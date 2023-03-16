4b941394d8b5ba1c3dae6764deb61b188a346d62
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-03-16 18:12:21 +0100

    WIP Spring security

--- kolmanfreecss.gateway.scg/src/main/java/com/kolmanfreecss/config/ConfigGateway.java
+++ kolmanfreecss.gateway.scg/src/main/java/com/kolmanfreecss/config/ConfigGateway.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class ConfigGateway {
@@ -11,13 +13,17 @@ public class ConfigGateway {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-//                .route(p -> p
-//                        .path("/get")
-//                        .filters(f -> f.addRequestHeader("Hello", "World"))
-//                        .uri("http://httpbin.org:80"))
                 .route("paymentId", r -> r.path("/api/payment/**").uri("http://localhost:8763")) //static routing
                 .route("orderId", r -> r.path("/api/order/**").uri("http://localhost:8762")) //dynamic routing
                 .build();
     }
 
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/api/**").authenticated()
+                .anyExchange().permitAll()
+                .and().build();
+    }
+    
 }
